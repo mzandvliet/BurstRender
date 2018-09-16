1bd9c688a669ffdd3a2f579c0b5ac06df39c7102
m_zandvliet <martijn@ramjetanvil.com>
2018-09-16 15:06:41 +0200

    Updated RNG performance metrics with new Unity.Mathematics.Random type

--- Assets/Scripts/Noise/NoiseTest.cs
+++ Assets/Scripts/Noise/NoiseTest.cs
@@ -4,6 +4,13 @@ using Unity.Jobs;
 using Unity.Collections;
 using Unity.Mathematics;
 
+/* Todo: Unity.Mathematics.Random was just introduced
+    Threadsafe, only uses single uint of state, has generators
+    for many types we want.
+
+    Note that here too, burst jobs are much faster the **second time** they run.
+ */
+
 public class NoiseTest : MonoBehaviour {
     Texture2D _tex;
 
@@ -11,6 +18,16 @@ public class NoiseTest : MonoBehaviour {
     const int numVals = res * res;
 
     private void Start() {
+        ProfileRNGs();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            ProfileRNGs();
+        }
+    }
+
+    private void TestXorShiftBurst() {
         XorshiftBurst xor = new XorshiftBurst(5132512, 3292391, 109854, 587295);
         for (int i = 0; i < 128; i++) {
             Debug.Log(xor.NextInt(0, 128));
@@ -18,11 +35,11 @@ public class NoiseTest : MonoBehaviour {
         }
     }
 
-    // Todo: Unity.Mathematics.Random was just introduced
+    private void ProfileRNGs() {
+        // Time it takes for many RNG implementations to fill a buffer with random 32 bit floats with distribution [0,1)
+        // For each RNG I list two times measures on my machine: editor with safetychecks, build without safetychecks
+        // If burst job is used, it is single-threaded. Obviously, using multiple threads with multiple RNGs is faster.
 
-    private void Profile() {
-        _tex = new Texture2D(res, res, TextureFormat.ARGB32, false, true);
-        var data = new Color[res * res];
         var values = new NativeArray<float>(numVals, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
 
         // 220ms, 55ms
@@ -40,7 +57,25 @@ public class NoiseTest : MonoBehaviour {
             values[i] = UnityEngine.Random.value;
         }
         sw.Stop();
-        Debug.Log("Unity.Random.value: " + sw.ElapsedMilliseconds);
+        Debug.Log("Unity.Random: " + sw.ElapsedMilliseconds);
+
+        // 245ms, 84ms
+        sw = System.Diagnostics.Stopwatch.StartNew();
+        var uMathRng = new Unity.Mathematics.Random(1234);
+        for (int i = 0; i < values.Length; i++) {
+            values[i] = uMathRng.NextFloat();
+        }
+        sw.Stop();
+        Debug.Log("Unity.Mathematics.Random: " + sw.ElapsedMilliseconds);
+
+        // 248ms, 10ms
+        sw = System.Diagnostics.Stopwatch.StartNew();
+        var umrj = new UMathRngJob();
+        umrj.Random = uMathRng;
+        umrj.Values = values;
+        umrj.Schedule().Complete();
+        sw.Stop();
+        Debug.Log("Unity.Mathematics.Random BurstJob: " + sw.ElapsedMilliseconds);
 
         // 226ms, 71ms
         var rm = new Meisui.Random.MersenneTwister(1234);
@@ -49,7 +84,7 @@ public class NoiseTest : MonoBehaviour {
             values[i] = (float)rm.genrand_real2();
         }
         sw.Stop();
-        Debug.Log("MT Managed: " + sw.ElapsedMilliseconds);
+        Debug.Log("MersenneTwister Managed: " + sw.ElapsedMilliseconds);
 
         // 351ms, 91ms
         var rrm = new RamjetMath.MersenneTwister(1234);
@@ -58,7 +93,7 @@ public class NoiseTest : MonoBehaviour {
             values[i] = rrm.genrand_real2();
         }
         sw.Stop();
-        Debug.Log("MT Burst: " + sw.ElapsedMilliseconds);
+        Debug.Log("MersenneTwister Burst: " + sw.ElapsedMilliseconds);
 
         // 359ms, 15ms
         sw = System.Diagnostics.Stopwatch.StartNew();
@@ -68,7 +103,7 @@ public class NoiseTest : MonoBehaviour {
         mtj.Schedule().Complete();
         sw.Stop();
         rrm.Dispose();
-        Debug.Log("MT Burst Job: " + sw.ElapsedMilliseconds);
+        Debug.Log("MersenneTwister BurstJob: " + sw.ElapsedMilliseconds);
 
         // 162ms, 76
         var xor = new Xorshift(1234);
@@ -77,7 +112,7 @@ public class NoiseTest : MonoBehaviour {
             values[i] = xor.NextFloat();
         }
         sw.Stop();
-        Debug.Log("XOR Managed: " + sw.ElapsedMilliseconds);
+        Debug.Log("XORShift Managed: " + sw.ElapsedMilliseconds);
 
         // 1107, 21ms
         sw = System.Diagnostics.Stopwatch.StartNew();
@@ -87,27 +122,33 @@ public class NoiseTest : MonoBehaviour {
         xorj.Random = xorb;
         xorj.Schedule().Complete();
         sw.Stop();
-        Debug.Log("XOR Burst Job: " + sw.ElapsedMilliseconds);
+        Debug.Log("XORShift BurstJob: " + sw.ElapsedMilliseconds);
 
 
-        float min = 2f;
-        float max = -1f;
+        // Find min and max values in buffer, and turn it into a texture for visual inspection
+        // Todo: do this for all RNGs, not just for the last one to generate values.
 
-        for (int i = 0; i < numVals; i++) {
-            float val = values[i];
-            if (val > max) {
-                max = val;
-            }
-            if (val < min) {
-                min = val;
-            }
-            data[i] = new Color(val, val, val, 1f);
-        }
+        // float min = 2f;
+        // float max = -1f;
 
-        Debug.Log(min + ", " + max);
+        // _tex = new Texture2D(res, res, TextureFormat.ARGB32, false, true);
+        // var data = new Color[res * res];
 
-        _tex.SetPixels(0, 0, res, res, data, 0);
-        _tex.Apply();
+        // for (int i = 0; i < numVals; i++) {
+        //     float val = values[i];
+        //     if (val > max) {
+        //         max = val;
+        //     }
+        //     if (val < min) {
+        //         min = val;
+        //     }
+        //     data[i] = new Color(val, val, val, 1f);
+        // }
+
+        // Debug.Log(min + ", " + max);
+
+        // _tex.SetPixels(0, 0, res, res, data, 0);
+        // _tex.Apply();
 
         values.Dispose();
     }
@@ -115,6 +156,18 @@ public class NoiseTest : MonoBehaviour {
 
     private void OnGUI() {
         GUI.DrawTexture(new Rect(0f, 0f, res, res), _tex);
+    }
+
+    [BurstCompile]
+    public struct UMathRngJob : IJob {
+        [WriteOnly] public NativeArray<float> Values;
+        public Unity.Mathematics.Random Random;
+
+        public void Execute() {
+            for (int i = 0; i < Values.Length; i++) {
+                Values[i] = Random.NextFloat();
+            }
+        }
     }
 
     [BurstCompile]
