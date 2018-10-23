9baad09130d92dc7bbee628098db2b18955c0c0a
m_zandvliet <martijn@ramjetanvil.com>
2018-10-23 18:58:37 +0200

    It's... something

--- Assets/Scripts/Paint/Painter.cs
+++ Assets/Scripts/Paint/Painter.cs
@@ -11,7 +11,13 @@ using Ramjet;
     
     Draw along a spline on a canvas of.. pixels for now.
     
-    Having analytic projection from 3d cubic curve to 2d screenspace cubic curve will be instrumental
+    Ideas:
+
+    - Need to draw actual ink/paint. Needs to be smooth, anti-aliased.
+        - GPU based 2d mesh along the splines I guess? And then rasterize that.
+    - Having analytic projection from 3d cubic curve to 2d screenspace cubic curve will be useful
+    - Can use distance fields to model scenes including curvature/normal information
+    - Scene geometry should express how it wants to be drawn. Saves analysis.
 
  */
 
@@ -24,29 +30,29 @@ public class Painter : MonoBehaviour {
     private NativeArray<float3> _canvas;
     private Texture2D _canvasTex;
 
-    private const float CANVASSCALE = 10f;
-    private const int NUMCURVES = 32;
-    private const int CANVASRES = 512;
+    private const float CANVAS_SCALE = 10f;
+    private const int CANVAS_RES = 1024;
+    private const int NUM_CURVES = 64;
 
     private void Awake() {
-        _curves = new NativeArray<float2>(NUMCURVES * 4, Allocator.Persistent);
-        _colors = new NativeArray<float3>(NUMCURVES, Allocator.Persistent);
+        _curves = new NativeArray<float2>(NUM_CURVES * 4, Allocator.Persistent);
+        _colors = new NativeArray<float3>(NUM_CURVES, Allocator.Persistent);
         _distanceCache = new NativeArray<float>(32, Allocator.Persistent);
 
         _rng = new Rng(1234);
 
-        for (int i = 0; i < NUMCURVES; i++) {
+        for (int i = 0; i < NUM_CURVES; i++) {
             float2 p = new float2(_rng.NextFloat(1f, 9f), _rng.NextFloat(0.5f, 1f));
             for (int j = 0; j < 4; j++) {
                 _curves[i * 4 + j] = p;
-                p += new float2(_rng.NextFloat(-0.3f, 0.3f), _rng.NextFloat(0.5f, 1.2f)) * j;
+                p += new float2(_rng.NextFloat(-0.3f, 0.3f), _rng.NextFloat(0.8f, 1.6f)) * j;
             }
 
             _colors[i] = new float3(_rng.NextFloat(0.3f, 0.5f),_rng.NextFloat(0.6f, 0.8f),_rng.NextFloat(0.3f, 0.6f));
         }
 
-        _canvas = new NativeArray<float3>(CANVASRES * CANVASRES, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-        _canvasTex = new Texture2D(CANVASRES, CANVASRES, TextureFormat.ARGB32, false, true);
+        _canvas = new NativeArray<float3>(CANVAS_RES * CANVAS_RES, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+        _canvasTex = new Texture2D(CANVAS_RES, CANVAS_RES, TextureFormat.ARGB32, false, true);
 
         var ccj = new ClearCanvasJob();
         ccj.canvas = _canvas;
@@ -68,21 +74,22 @@ public class Painter : MonoBehaviour {
         }
 
         var h = new JobHandle();
-        for (int i = 0; i < NUMCURVES; i++) {
+        for (int i = 0; i < NUM_CURVES; i++) {
             var sj = new StrokeJob();
             sj.canvas = _canvas;
             sj.curve = _curves;
+            sj.distCache = _distanceCache;
             sj.steps = steps;
             sj.curveIndex = i;
             sj.color = _colors[i];
             sj.time = Time.frameCount;
-            sj.canvasResolution = CANVASRES;
-            sj.canvasScale = CANVASSCALE;
+            sj.canvasResolution = CANVAS_RES;
+            sj.canvasScale = CANVAS_SCALE;
             h = sj.Schedule(h);
         }
         h.Complete();
 
-        Util.ToTexture2D(_canvas, _canvasTex, new int2(CANVASRES, CANVASRES));
+        Util.ToTexture2D(_canvas, _canvasTex, new int2(CANVAS_RES, CANVAS_RES));
     }
 
     private void OnGUI() {
@@ -125,6 +132,7 @@ public class Painter : MonoBehaviour {
     [BurstCompile]
     public struct StrokeJob : IJob {
         [ReadOnly] public NativeArray<float2> curve;
+        [ReadOnly] public NativeArray<float> distCache;
         public NativeArray<float3> canvas;
         public float3 color;
         public int curveIndex;
@@ -142,21 +150,30 @@ public class Painter : MonoBehaviour {
             float2 p = BDC2Cube.GetAt(curve, t, curveIndex * 4);
             float2 n = BDC2Cube.GetNormalAt(curve, t, curveIndex * 4);
 
-            int2 pidx = ToGrid(p, canvasScale, canvasResolution);
-            int2 pidxl = ToGrid(p + n * 0f, canvasScale, canvasResolution);
-            int2 pidxr = ToGrid(p + n * 0f, canvasScale, canvasResolution);
+            int widthSteps = 4 + (int)math.round((1f - t) * 32f);
+            float width = 0.33f;
+            float widthStep = width / (float)(widthSteps-1);
+            float2 start = p - n * (width*0.5f);
 
-            Draw(pidx, canvas, color, canvasResolution);
-            Draw(pidxl, canvas, color, canvasResolution);
-            Draw(pidxr, canvas, color, canvasResolution);
+            for (int j = 0; j < widthSteps; j++) {
+                float alpha = RampUpDown(j, widthSteps);
+                int2 pi = ToGrid(start + n * (widthStep * j), canvasScale, canvasResolution);
+                Draw(pi, canvas, color, alpha, canvasResolution);
+            }
+        }
+
+        private static float RampUpDown(int i, int max) {
+            float halfMax = (max-1) * 0.5f;
+            return i <= halfMax ? i / halfMax : 2f - (i / halfMax);
         }
 
         private static int2 ToGrid(float2 p, float canvasScale, int canvasRes) {
             return (int2)math.round(p / canvasScale * canvasRes);
         }
 
-        private static void Draw(int2 p, NativeArray<float3> canvas, float3 col, int res)  {
-            canvas[p.y * res + p.x] = col;
+        private static void Draw(int2 p, NativeArray<float3> canvas, float3 col, float alpha, int res)  {
+            var current = canvas[p.y * res + p.x];
+            canvas[p.y * res + p.x] = math.lerp(current, col, alpha);
         }
     }
 }
